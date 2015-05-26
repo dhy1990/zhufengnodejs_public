0175c246f8982946ed6d644c8dd4697fae0fe6fd
zhangrenyang-t510 <zhang_renyang@>
2015-05-26 21:48:03 +0800

    redis

--- 13.redis/1.redis.js
+++ 13.redis/1.redis.js
@@ -3,12 +3,16 @@ var client = redis.createClient(6379,'localhost');
 client.on('error',function(err){
     console.log(err);
 });
-client.set('name','zzzzzz',function(err,result){
-    console.log(arguments);
+client.select(1,function(){
+    client.set('name','zzzzzz',function(err,result){
+        console.log(arguments);
+    });
 });
-//存入
+
+//瀛樺叆
 /*
+npm install redis
 key = person_100
    name tom
    age 100
- 再全部获取并输出*/
+ 鍐嶅叏閮ㄨ幏鍙栧苟杈撳嚭*/
