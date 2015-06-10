786224eba789935544d1a1413ef60343d58d916d
zhangrenyang-t510 <zhang_renyang@>
2015-06-10 21:22:17 +0800

    grunt

--- 15.grunt/first/Gruntfile.js
+++ 15.grunt/first/Gruntfile.js
@@ -1,20 +1,23 @@
 /**
- * 如何触发警告和致命错误
+ * 如何定会义多任务
  * @param grunt
  */
 
 module.exports = function(grunt){
- grunt.registerTask('interview',function(status){
-    if(status == 'has_boy'){
-        //用C语言写个红黑树
-        grunt.warn('write a red-black tree with c');
-    }else if(status == 'married'){
-        //用汇编写个红黑树
-        grunt.fatal('write a red-black tree with assembly langular');
-    }else{
-        //用word文档写个hello world
-        grunt.log.writeln('write hello world with word');
-    }
-    grunt.log.write('you have been accepted');//你来上班吧
- });
+  //初始化配置信息，可以定义属性和值
+   grunt.initConfig({
+       eat:{//eat是一个task的名字
+            food:'rice',//里面是一个target的名字  目标
+            drink:'orange',//
+            vegatable:'potato'//
+        }
+   });
+
+    grunt.registerMultiTask('eat',function(){
+        for(var i=0;i<10;i++){
+            grunt.log.writeln
+            ('I am eating '+i+' of '+this.target+","+this.data);
+        }
+    });
+
 }
