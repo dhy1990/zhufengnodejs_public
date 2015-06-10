9a2b24207529b6ffcb50175b47dc3ed4b078054e
zhangrenyang-t510 <zhang_renyang@>
2015-06-10 21:34:07 +0800

    grunt

--- 15.grunt/first/Gruntfile.js
+++ 15.grunt/first/Gruntfile.js
@@ -1,23 +1,27 @@
 /**
- * 如何定会义多任务
+ * 如何读写文件
  * @param grunt
  */
-
+/**
+ *
+ *
+ */
 module.exports = function(grunt){
-  //初始化配置信息，可以定义属性和值
-   grunt.initConfig({
-       eat:{//eat是一个task的名字
-            food:'rice',//里面是一个target的名字  目标
-            drink:'orange',//
-            vegatable:'potato'//
-        }
-   });
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json')
+    });
+    /**
+     * 往这个文件里增加我的注释
+     * // 项目名 作者
+     * var content = grunt.file.read('./test.js','utf8');
+     * var comment = '<%=pkg.name%> <%=pkg.author%>';
+     * comment = grunt.template.process(comment);
+     * 把变化后的内容写入test2.js
+     *
+     *
+     */
+    grunt.registerTask('after',function(){
 
-    grunt.registerMultiTask('eat',function(){
-        for(var i=0;i<10;i++){
-            grunt.log.writeln
-            ('I am eating '+i+' of '+this.target+","+this.data);
-        }
     });
 
 }
