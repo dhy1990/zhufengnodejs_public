9ca3a0ef2de241b175de812986dc0cccc223f9da
zhangrenyang-t510 <zhang_renyang@>
2015-07-02 21:08:56 +0800

    上传

--- 18.page/server.js
+++ 18.page/server.js
@@ -1,3 +1,15 @@
-/**
- * Created by Administrator on 2015-07-02.
- */
+var http = require('http');
+//Request URL:https://www.baidu.com/
+//Request Method:GET
+var fs = require('fs');
+var server = http.createServer(function(req,res){
+    var method = req.method;
+    var url = req.url;
+    if(url == '/'){
+        fs.createReadStream('./index.html').pipe(res);
+    }else{
+
+    }
+});
+
+server.listen(80);
