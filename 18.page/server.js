6641fa0e0f9fc89c1a8ca8495c779eb134380895
zhangrenyang-t510 <zhang_renyang@>
2015-07-02 21:15:51 +0800

    上传

--- 18.page/server.js
+++ 18.page/server.js
@@ -2,13 +2,22 @@ var http = require('http');
 //Request URL:https://www.baidu.com/
 //Request Method:GET
 var fs = require('fs');
+var mime = require('mime');
+var path = require('path');
 var server = http.createServer(function(req,res){
     var method = req.method;
     var url = req.url;
     if(url == '/'){
         fs.createReadStream('./index.html').pipe(res);
     }else{
-
+        res.writeHead(200,{'Content-Type':mime.lookup(url)});
+        fs.exists(path.join(__dirname,url),function(exists){
+            if(exists){
+                fs.createReadStream(path.join(__dirname,url)).pipe(res);
+            }else{
+                res.end('404');
+            }
+        });
     }
 });
 
